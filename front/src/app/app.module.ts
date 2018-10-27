eecba43c5abdd1146416a8ffbd09f57f7ed299ef
c10t <c10t@fa.ke>
2018-10-28 04:59:01 +0900

    show sidenav content

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TopbarComponent } from './topbar/topbar.component';
+import { FacetComponent } from './facet/facet.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopbarComponent
+    TopbarComponent,
+    FacetComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatSidenavModule,
     MatToolbarModule
   ],
   providers: [],
